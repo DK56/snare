71e58e25038855a2e5717ffec3543a95348b43a2
Tobias Viehmann <tobias.viehmann@mail.de>
2020-01-27 03:13:25 +0100

    Add 'setup.py' and 'requirements.txt'

---
+++ setup.py
@@ -0,0 +1,31 @@
+import io
+import os
+from setuptools import setup
+
+dir_path = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(dir_path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='snare',
+    version='0.1.0',
+    description='Scorebased Neural Architecture REduction',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tobias Viehmann',
+    author_email='tobias.viehmann@mail.de',
+    python_requires='>=3.6.0',
+    url='https://github.com/dk56/snare',
+    py_modules=['snare'],
+    install_requires=['numpy>=1.16.0',
+                      'tensorflow-gpu==1.14.0'],
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
