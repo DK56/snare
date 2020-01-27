9b4c6134b127ea5b2a4c167ceabd52223e2321bd
Tobias Viehmann <tobias.viehmann@mail.de>
2020-01-27 04:11:15 +0100

    Add LeNet-300-100 example

---
+++ examples/lenet300_100.py
@@ -0,0 +1,62 @@
+
+"""
+# This script is an easy implementation of a LeNet-300-100 architecture
+from http://yann.lecun.com/exdb/publis/pdf/lecun-01a.pdf. It has minor
+modifications to the proposed architecture.
+
+"""
+
+from snare import Snare
+from tensorflow.python import keras
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras import losses
+from tensorflow.python.keras import layers
+from tensorflow.python.keras.datasets import mnist
+
+
+def prepare_dataset():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.astype("float32")
+    x_test = x_test.astype("float32")
+
+    # Scale all values
+    x_train /= 255
+    x_test /= 255
+
+    # One-hot encoding of labels
+    y_train = keras.utils.to_categorical(y_train, 10)
+    y_test = keras.utils.to_categorical(y_test, 10)
+
+    # Reshape input images
+    x_train = x_train.reshape(x_train.shape[0], 784)
+    x_test = x_test.reshape(x_test.shape[0], 784)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+# Define model architecture
+lenet5 = Sequential()
+lenet5.add(layers.Dense(300, input_shape=(784,), activation="tanh"))
+lenet5.add(layers.Dense(100, activation="tanh"))
+lenet5.add(layers.Dense(10, activation="softmax"))
+
+# Compile model
+compile_args = {'loss': losses.categorical_crossentropy,
+                'optimizer': 'SGD', 'metrics': ['accuracy']}
+lenet5.compile(**compile_args)
+lenet5.summary()
+
+# Train model
+batch_size = 128
+dataset = prepare_dataset()
+(x_train, y_train), (x_test, y_test) = dataset
+lenet5.fit(x=x_train, y=y_train, epochs=200, batch_size=batch_size,
+           validation_data=(x_test, y_test), verbose=1)
+
+snare = Snare(lenet5, compile_args)
+result = snare.reduce(dataset, 0.005)
+
+test_score = result.evaluate(x_test, y_test)
+print("LeNet5: loss: {:.4f}, accuracy: {:.4f}".format(
+    test_score[0], test_score[1]))
