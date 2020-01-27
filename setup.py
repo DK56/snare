019199bc8a7dbd5fe8e430d9be418b053982ba52
Tobias Viehmann <tobias.viehmann@mail.de>
2020-01-27 04:02:04 +0100

    Update package-name to 'snare-ml'

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 import io
 import os
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 dir_path = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(dir_path, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='snare',
+    name='snare-ml',
     version='0.1.0',
     description='Scorebased Neural Architecture REduction',
     long_description=long_description,
-    long_description_content_type='text/markdown',
     author='Tobias Viehmann',
     author_email='tobias.viehmann@mail.de',
     python_requires='>=3.6.0',
     url='https://github.com/dk56/snare',
-    py_modules=['snare'],
+    packages=find_namespace_packages(include=['snare.*']),
     install_requires=['numpy>=1.16.0',
                       'tensorflow-gpu==1.14.0'],
     license='MIT',
